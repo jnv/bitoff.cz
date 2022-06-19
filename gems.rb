6f478b752339b4c3c16c9036172b77450c0120b4
Jan Vlnas <616767+jnv@users.noreply.github.com>
2022-06-19 21:58:11 +0000

    Lock gem versions

--- gems.rb
+++ gems.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
 
-gem 'github-pages', group: :jekyll_plugins
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-# TODO: Remove for Jekyll 3.7
-group :jekyll_plugins do
-    gem "jekyll-livereload"
-end
+gem 'github-pages', '~> 226', group: :jekyll_plugins
 
 group :development do
   gem "forematter", git: "https://github.com/jnv/forematter", require: false
